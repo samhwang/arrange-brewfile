0def1f959185beec99efb49c70bca573eaabc47a
Sam Huynh <samhwang2112.dev@gmail.com>
2023-07-05 11:27:33 +1000

    feat: refactor brew structure to be a Record and Keys

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,67 +1,78 @@
 import { describe, it, expect, vi } from 'vitest';
-import { parseBrewStructure, buildOutputContent } from './index';
+import { parseBrewStructure, buildOutputContent, BrewStructure } from './index';
 
 const consoleErrorSpy = vi.spyOn(console, 'error');
 consoleErrorSpy.mockImplementation(() => {});
 
+function getBrewStructure(output: Partial<BrewStructure> = {}) {
+  const emptyOutput: BrewStructure = {
+    taps: [],
+    brews: [],
+    casks: [],
+    vscodes: [],
+  };
+
+  return Object.assign(emptyOutput, output);
+}
+
 describe('parseBrewStructure', () => {
   it("Should return empty if input doesn't have content", () => {
     const input = [''];
-    const expected = [[], [], [], []];
+    const expected = getBrewStructure();
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add a tap', () => {
     const input = ['tap "aws/tap"'];
-    const expected = [['tap "aws/tap"'], [], [], []];
+    const expected = getBrewStructure({ taps: ['tap "aws/tap"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add and sort taps', () => {
     const input = ['tap "beeftornado/rmtree"', 'tap "aws/tap"'];
-    const expected = [['tap "aws/tap"', 'tap "beeftornado/rmtree"'], [], [], []];
+    const expected = getBrewStructure({ taps: ['tap "aws/tap"', 'tap "beeftornado/rmtree"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add a brew', () => {
     const input = ['brew "ack"'];
-    const expected = [[], ['brew "ack"'], [], []];
+    const expected = getBrewStructure({ brews: ['brew "ack"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add and sort brews', () => {
     const input = ['brew "xz"', 'brew "ack"'];
-    const expected = [[], ['brew "ack"', 'brew "xz"'], [], []];
+    const expected = getBrewStructure({ brews: ['brew "ack"', 'brew "xz"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add a cask', () => {
     const input = ['cask "fork"'];
-    const expected = [[], [], ['cask "fork"'], []];
+    const expected = getBrewStructure({ casks: ['cask "fork"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add and sort casks', () => {
     const input = ['cask "fork"', 'cask "canva"'];
-    const expected = [[], [], ['cask "canva"', 'cask "fork"'], []];
+    const expected = getBrewStructure({ casks: ['cask "canva"', 'cask "fork"'] });
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add vscode package', () => {
     const input = ['vscode "rome.rome"'];
-    const expected = [[], [], [], ['vscode "rome.rome"']];
+    const expected = getBrewStructure({vscodes: ['vscode "rome.rome"']});
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should add and sort vscode packages', () => {
     const input = ['vscode "rome.rome"', 'vscode "esbenp.prettier"'];
-    const expected = [[], [], [], ['vscode "esbenp.prettier"','vscode "rome.rome"']];
+    const expected = getBrewStructure({vscodes: ['vscode "esbenp.prettier"', 'vscode "rome.rome"']});
     expect(parseBrewStructure(input)).toEqual(expected);
   });
 
   it('Should ignore invalid lines', () => {
     const input = ['blah "blah.blah"'];
-    const expected = [[], [], [], []];
+    const expected = getBrewStructure();
     expect(parseBrewStructure(input)).toEqual(expected);
     expect(consoleErrorSpy).toBeCalled();
   });
@@ -69,61 +80,66 @@ describe('parseBrewStructure', () => {
 
 describe('buildOutputContent', () => {
   it('Should return blank if input is empty', () => {
-    const input = [[], [], []];
+    const input = getBrewStructure();
     const expected = '';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have a tap', () => {
-    const input = [['tap "aws/tap"'], [], []];
+    const input = getBrewStructure({ taps: ['tap "aws/tap"'] });
     const expected = 'tap "aws/tap"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have many taps', () => {
-    const input = [['tap "aws/tap"', 'tap "beeftornado/rmtree"'], [], []];
+    const input = getBrewStructure({ taps: ['tap "aws/tap"', 'tap "beeftornado/rmtree"'] });
     const expected = 'tap "aws/tap"\ntap "beeftornado/rmtree"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have a brew', () => {
-    const input = [[], ['brew "ack"'], []];
+    const input = getBrewStructure({ brews: ['brew "ack"'] });
     const expected = 'brew "ack"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have many brews', () => {
-    const input = [[], ['brew "ack"', 'brew "xz"'], []];
+    const input = getBrewStructure({ brews: ['brew "ack"', 'brew "xz"'] });
     const expected = 'brew "ack"\nbrew "xz"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have a cask', () => {
-    const input = [[], [], ['cask "fork"']];
+    const input = getBrewStructure({ casks: ['cask "fork"'] });
     const expected = 'cask "fork"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have many casks', () => {
-    const input = [[], [], ['cask "canva"', 'cask "fork"']];
+    const input = getBrewStructure({ casks: ['cask "canva"', 'cask "fork"'] });
     const expected = 'cask "canva"\ncask "fork"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
   it('Should have a vscode package', () => {
-    const input = [[], [], [], ['vscode "rome.rome"']];
+    const input = getBrewStructure({vscodes: ['vscode "rome.rome"']});
     const expected = 'vscode "rome.rome"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
 
-  it('Should have many packages', () => {
-    const input = [[], [], [], ['vscode "esbenp.prettier"', 'vscode "rome.rome"']];
+  it('Should have many vscode packages', () => {
+    const input = getBrewStructure({vscodes: ['vscode "esbenp.prettier"', 'vscode "rome.rome"']});
     const expected = 'vscode "esbenp.prettier"\nvscode "rome.rome"';
     expect(buildOutputContent(input)).toEqual(expected);
-  })
+  });
 
   it('Should have 1 tap 1 brew 1 cask with a newline between them', () => {
-    const input = [['tap "aws/tap"'], ['brew "ack"'], ['cask "canva"'], ['vscode "rome.rome"']];
+    const input = getBrewStructure({
+      taps: ['tap "aws/tap"'],
+      brews: ['brew "ack"'],
+      casks: ['cask "canva"'],
+      vscodes: ['vscode "rome.rome"']
+    });
     const expected = 'tap "aws/tap"\n\nbrew "ack"\n\ncask "canva"\n\nvscode "rome.rome"';
     expect(buildOutputContent(input)).toEqual(expected);
   });
--- src/index.ts
+++ src/index.ts
@@ -8,64 +8,70 @@ export function parseFile(inputPath: string) {
   return content.split('\n');
 }
 
+type Keys = 'taps' | 'brews' | 'casks' | 'vscodes';
+type Block = string[];
+export type BrewStructure = Record<Keys, Block>;
+
+const tapLine = z.string().startsWith('tap "');
+const brewLine = z.string().startsWith('brew "');
+const caskLine = z.string().startsWith('cask "');
+const vscodeLine = z.string().startsWith('vscode "');
+
 export function parseBrewStructure(input: string[]) {
-  const taps: string[] = [];
-  const brews: string[] = [];
-  const casks: string[] = [];
-  const vscodes: string[] = [];
-
-  input.forEach((line) => {
-    if (!line || line.length === 0) {
-      return;
-    }
-
-    const tapLine = z.string().startsWith('tap "');
-    const isTapLine = tapLine.safeParse(line);
-    if (isTapLine.success) {
-      taps.push(isTapLine.data);
-      taps.sort();
-      return;
-    }
-
-    const brewLine = z.string().startsWith('brew "');
-    const isBrewLine = brewLine.safeParse(line);
-    if (isBrewLine.success) {
-      brews.push(isBrewLine.data);
-      brews.sort();
-      return;
-    }
-
-    const caskLine = z.string().startsWith('cask "');
-    const isCaskLine = caskLine.safeParse(line);
-    if (isCaskLine.success) {
-      casks.push(line);
-      casks.sort();
-      return;
-    }
-
-    const vscodeLine = z.string().startsWith('vscode "');
-    const isVSCodeLine = vscodeLine.safeParse(line);
-    if (isVSCodeLine.success) {
-      vscodes.push(line);
-      vscodes.sort();
-      return;
-    }
-
-    console.error('INVALID LINE. SKIPPING!');
-    return;
-  });
-
-  return [taps, brews, casks, vscodes];
-}
-
-export function buildOutputContent(structure: string[][]) {
-  return structure
-    .reduce((accumulator, part) => {
-      if (part.length === 0) {
+  return input.reduce<BrewStructure>(
+    (accumulator, line) => {
+      if (!line || line.length === 0) {
         return accumulator;
       }
 
-      const contentBlock = part.reduce((block, line) => {
+      const isTapLine = tapLine.safeParse(line);
+      if (isTapLine.success) {
+        accumulator.taps.push(isTapLine.data);
+        accumulator.taps.sort();
+        return accumulator;
+      }
+
+      const isBrewLine = brewLine.safeParse(line);
+      if (isBrewLine.success) {
+        accumulator.brews.push(isBrewLine.data);
+        accumulator.brews.sort();
+        return accumulator;
+      }
+
+      const isCaskLine = caskLine.safeParse(line);
+      if (isCaskLine.success) {
+        accumulator.casks.push(line);
+        accumulator.casks.sort();
+        return accumulator;
+      }
+
+      const isVSCodeLine = vscodeLine.safeParse(line);
+      if (isVSCodeLine.success) {
+        accumulator.vscodes.push(line);
+        accumulator.vscodes.sort();
+        return accumulator;
+      }
+
+      console.error('INVALID LINE. SKIPPING!');
+      return accumulator;
+    },
+    {
+      taps: [],
+      brews: [],
+      casks: [],
+      vscodes: [],
+    }
+  );
+}
+
+export function buildOutputContent(structure: BrewStructure) {
+  return Object.entries(structure)
+    .reduce((accumulator, [key, values]: [string, string[]]) => {
+      if (values.length === 0) {
+        return accumulator;
+      }
+
+      const contentBlock = values.reduce((block, line) => {
         return `${block}\n${line}`;
       }, '');
       return `${accumulator}${contentBlock}\n`;
